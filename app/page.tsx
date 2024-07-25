18844da83f86e25abaae7485ab19198e3aa7a2cc
alvropena <me@alvropena.com>
2024-07-24 20:48:42 -0500

    Added Fonts and cleared boilerplate

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Page() {
   return (
-    <div>Proarandanos</div>
+    <main className=''>
+      <h1 className='text-5xl font-display font-bold'>Hello World!</h1>
+    </main>
   )
 }
