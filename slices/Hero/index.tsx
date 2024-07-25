fda13a799b879d2088ced885be7839657dbe3b58
alvropena <me@alvropena.com>
2024-07-24 22:44:51 -0500

    Added Hero Slice

---
+++ slices/Hero/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Content } from "@prismicio/client";
+import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+
+export type HeroProps = SliceComponentProps<Content.HeroSlice>;
+
+const Hero = ({ slice }: HeroProps): JSX.Element => {
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <PrismicRichText field={slice.primary.title} />
+      <PrismicRichText field={slice.primary.body} />
+      <PrismicNextLink field={slice.primary.button_link}>
+        {slice.primary.button_text}
+      </PrismicNextLink>
+      <PrismicNextImage field={slice.primary.image} />
+    </section>
+  );
+};
+
+export default Hero;
