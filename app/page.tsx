b2009c97ae854cc53fc00ad996c119a2c148c393
alvropena <me@alvropena.com>
2024-07-25 03:44:02 -0500

    Add Snapshot to Hero Slice

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
-import React from "react";
 import { Metadata } from "next";
 import { SliceZone } from "@prismicio/react";
-import { createClient } from "../prismicio";
-import { components } from "../slices";
+import { createClient } from "@/prismicio";
+import { components } from "@/slices";
 
 export default async function Page() {
   const client = createClient();
   const page = await client.getSingle("homepage");
-
-  return <div className="text-red-500">It worked!</div>
 
   return <SliceZone slices={page.data.slices} components={components} />;
 }
